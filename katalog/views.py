23e23d516306632ce87beb2dc0123f9262fd346e
syafiqoarashy <101589777+syafiqoarashy@users.noreply.github.com>
2022-09-14 11:21:27 +0700

    completed assignment 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    data_katalog_item = CatalogItem.objects.all()
+    context = {
+        'list_item': data_katalog_item,
+        'name': 'Syafiqo Arashy Octaviano',
+        'studentid': '2106657821'
+    }
+    return render(request, 'katalog.html', context)
