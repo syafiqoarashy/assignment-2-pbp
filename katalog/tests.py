a77c5b4f8049ccced052783f575f201a85b5a539
syafiqoarashy <101589777+syafiqoarashy@users.noreply.github.com>
2022-09-15 09:28:21 +0700

    added testing

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from django.urls import resolve
+from .views import show_katalog
 # Create your tests here.
+
+class KatalogTest(TestCase):
+    def test_contoh_app_url_exists(self):
+        response = Client().get('/katalog/')
+        self.assertEqual(response.status_code,200)
+    def test_contoh_app_use_template(self):
+        response = Client().get('/katalog/')
+        self.assertTemplateUsed(response,'katalog.html')
