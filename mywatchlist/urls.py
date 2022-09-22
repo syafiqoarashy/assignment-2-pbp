8875c36644e38c771aa5163e5c4db5c7a791c22f
syafiqoarashy <101589777+syafiqoarashy@users.noreply.github.com>
2022-09-22 08:43:28 +0700

    added empty path

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -8,9 +8,10 @@ from mywatchlist.views import show_data_json_id
 app_name = 'mywatchlist'
 
 urlpatterns = [
+    path('', show_mywatchlist, name='show_mywatchlist'),
     path('html/', show_mywatchlist, name='show_mywatchlist'),
-    path('xml/', show_data_xml, name='show_xml'),
-    path('json/', show_data_json, name='show_json'),
+    path('xml/', show_data_xml, name='show_data_xml'),
+    path('json/', show_data_json, name='show_data_json'),
     path('json/<int:id>', show_data_json_id, name='show_data_jason_id'),
     path('xml/<int:id>', show_data_xml_id, name='show_data_xml_id'),
 ]
